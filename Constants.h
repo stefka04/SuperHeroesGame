f0445af7d9ed5c701f2c7142f3c66fdb1aba7de0
Stefka Lyaskalieva <stefka1804@gmail.com>
2023-06-13 16:03:42 +0300

    Add constants

---
+++ Constants.h
@@ -0,0 +1,8 @@
+#pragma once
+const short MAX_USERNAME_SIZE = 16;
+const short INIT_CAPACITY = 8;
+const short MAX_BUFF_SIZE = 1024;
+
+const char usersFile[] = "users.txt";
+const char leaderboardFile[] = "leaderboard.txt";
+const char marketFile[] = "market.txt";

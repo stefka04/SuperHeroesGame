75907747c8be2e108b175230eb40bbc0d10ac673
Stefka Lyaskalieva <stefka1804@gmail.com>
2023-06-13 16:03:09 +0300

    Add enum classes

---
+++ EnumClasses.h
@@ -0,0 +1,19 @@
+#pragma once
+enum class AttackingMode
+{
+	Attacking,
+	Defensive
+};
+
+enum class StrengthTypes
+{
+	Fire,
+	Water,
+	Earth
+};
+
+enum class UserType
+{
+	Administrator,
+	Player
+};

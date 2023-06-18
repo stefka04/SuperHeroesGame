5fcff3a78e87dc9ed3044d0d78c99481103bb920
Stefka Lyaskalieva <stefka1804@gmail.com>
2023-06-18 23:54:21 +0300

    Update Constants

--- Constants.h
+++ Constants.h
@@ -6,3 +6,10 @@ const short MAX_BUFF_SIZE = 1024;
 const char usersFile[] = "users.txt";
 const char leaderboardFile[] = "leaderboard.txt";
 const char marketFile[] = "market.txt";
+
+const long x = 500;
+const long z = 1000;
+const long NEW_PERIOD_MONEY = 1500;           //j
+const long SEED_MONEY = 2000;  //m
+
+const unsigned MAX_MOVES = 3;
